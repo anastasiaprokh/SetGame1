fa72bee942cfe9e06af7fdb912266ed404a173b6
Matt Osborn <mattosborn@gmail.com>
2015-05-14 13:25:42 -0400

    Update ZetDeck.java

--- ZetDeck.java
+++ ZetDeck.java
@@ -6,18 +6,10 @@ public class ZetDeck extends Deck
   {
     super(81);
     for (int n = 1; n<=3; n++)
-    {
       for (int s = 1; s<=3; s++)
-      {
         for (int f = 1; f<=3; f++)
-        {
           for(int c = 1; c<=3; c++)
-          {
             super.add(new ZetCard(n,s,f,c));
-          }
-        }
-      }
-    }
   }
-
+  
 }
