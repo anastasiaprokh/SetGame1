2f3398e0a2de30f6e8e8452d720b6e38dfca8350
Matt Osborn <mattosborn@gmail.com>
2015-05-14 13:22:08 -0400

    Update ZetDeck.java

--- ZetDeck.java
+++ ZetDeck.java
@@ -1,20 +1,19 @@
-import java. util.*;
+import java.util.*;
 
 public class ZetDeck extends Deck
 {
-  private ArrayList <ZetCard> ZetDeck;
   public ZetDeck()
   {
     super(81);
-    for (int n = 1; n<4; n++)
+    for (int n = 1; n<=3; n++)
     {
-      for (int s = 1; s<4; s++)
+      for (int s = 1; s<=3; s++)
       {
-        for (int c = 1; c<4; c++)
+        for (int f = 1; f<=3; f++)
         {
-          for(int f = 1; f<4; f++)
+          for(int c = 1; c<=3; c++)
           {
-            ZetDeck.add(new ZetCard(n,s,c,f));
+            super.add(new ZetCard(n,s,f,c));
           }
         }
       }
